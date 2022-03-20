bf00922a748429250c69880f3385af284c33c72f
Valter Andrei <valterandrey@gmail.com>
2022-03-20 15:28:52 -0300

    update layout

--- src/components/Parking.vue
+++ src/components/Parking.vue
@@ -1,17 +1,40 @@
 <template>
-  <ol v-for="car in carList" :key="car.id">
-    <ul>
-      {{
-        car.plate
-      }}
-      <button class="button is-primary" @click="getCar(car.plate)">Ver reservas</button>
-    </ul>
-  </ol>
-  <div>
-    <ol v-for="(info, index) in carInfo.reservations" :key="index">
-      <li>Código: {{ info.code }}</li>
-      <li>Tempo: {{ info.time }}</li>
-    </ol>
+  <div class="table-container">
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>Placa</th>
+          <th>Saída</th>
+        </tr>
+      </thead>
+      <tbody v-for="car in carList" :key="car.id">
+        <tr>
+          <td>{{ car.plate }}</td>
+          <td>
+            <button class="button is-link is-light" @click="getCar(car.plate)">
+              Ver reservas
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="table-container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Entrada</th>
+        </tr>
+      </thead>
+      <tbody v-for="(info, index) in carInfo.reservations" :key="index">
+        <tr>
+          <td>{{ info.code }}</td>
+          <td>{{ info.created_at }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
