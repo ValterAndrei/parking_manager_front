f27bb1aaed1100b3df14e54564022272c1cd1944
Valter Andrei <valterandrey@gmail.com>
2022-03-26 12:23:15 -0300

    Refactor layout

--- src/components/Parking.vue
+++ src/components/Parking.vue
@@ -1,43 +1,51 @@
 <template>
   <section class="section">
     <div class="container">
-      <div class="field">
-        <label class="label">Placa</label>
-        <div class="control">
-          <input
-            ref="plate_input"
-            v-model="plate"
-            class="input"
-            type="text"
-            placeholder="e.g BQT-9377"
-          >
+      <div class="columns is-desktop">
+        <div class="column is-one-fifth">
+          <div class="field">
+            <label class="label">Placa</label>
+            <div class="control">
+              <input
+                ref="plate_input"
+                v-model="plate"
+                class="input"
+                type="text"
+                placeholder="e.g BQT-9377"
+              >
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <button
+                class="button is-link"
+                @click="checkin()"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="field is-grouped">
-        <div class="control">
-          <button
-            class="button is-link"
-            @click="checkin()"
-          >
-            Submit
-          </button>
+      <div class="columns">
+        <div class="column">
+          <div class="table-container">
+            <Car
+              :list="carList"
+              @on-show-car-info="getCar"
+            />
+          </div>
         </div>
-      </div>
-
-      <div class="table-container">
-        <Car
-          :list="carList"
-          @on-show-car-info="getCar"
-        />
-      </div>
-
-      <div class="table-container">
-        <Reservation
-          :list="reservationsList"
-          @make-payment="payment"
-          @make-checkout="checkout"
-        />
+        <div class="column">
+          <div class="table-container">
+            <Reservation
+              :list="reservationsList"
+              @make-payment="payment"
+              @make-checkout="checkout"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -45,32 +53,31 @@
 
 <script setup>
 import { ref } from "vue";
-import Car from "./Car.vue"
-import Reservation from "./Reservation.vue"
+import Car from "./Car.vue";
+import Reservation from "./Reservation.vue";
 
 const URL = process.env.VUE_APP_URL;
-const carList = ref([]);
+
+const carList          = ref([]);
 const reservationsList = ref([]);
-const plate = ref("");
-const plate_input = ref(null);
+const plate            = ref("");
+const plate_input      = ref(null);
 
 async function getCarList() {
   const response = await fetch(`${URL}`);
-  const data = await response.json();
+  const data     = await response.json();
 
   carList.value = data;
 }
 
 async function getCar(plate) {
   const response = await fetch(`${URL}/parking/${plate}`);
-  const data = await response.json();
+  const data     = await response.json();
 
   reservationsList.value = data.reservations;
 }
 
 async function checkin() {
-  // https://jasonwatmore.com/post/2020/04/30/vue-fetch-http-post-request-examples
-
   await fetch(`${URL}/parking/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -78,6 +85,7 @@ async function checkin() {
   });
 
   plate_input.value.focus();
+  plate.value = "";
 
   getCarList();
 }
