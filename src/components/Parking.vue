f3c1229427e924234d05a7db0dd620b4ea9b1088
Valter Andrei <valterandrey@gmail.com>
2022-04-03 14:55:01 -0300

    Fix indentation

--- src/components/Parking.vue
+++ src/components/Parking.vue
@@ -62,97 +62,97 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { toast } from "bulma-toast";
+  import { ref } from "vue";
+  import { toast } from "bulma-toast";
 
-import Car from "./Car.vue";
-import Reservation from "./Reservation.vue";
+  import Car from "./Car.vue";
+  import Reservation from "./Reservation.vue";
 
-const URL = process.env.VUE_APP_URL;
+  const URL = process.env.VUE_APP_URL;
 
-const carList          = ref([]);
-const reservationsList = ref([]);
-const plate            = ref("");
-const plate_input      = ref(null);
+  const carList          = ref([]);
+  const reservationsList = ref([]);
+  const plate            = ref("");
+  const plate_input      = ref(null);
 
-async function getCarList() {
-  const response = await fetch(`${URL}`);
-  const data     = await response.json();
+  async function getCarList() {
+    const response = await fetch(`${URL}`);
+    const data     = await response.json();
 
-  reservationsList.value = [] // Limpa lista de reservas.
-  carList.value = data;
-}
-
-async function getCar(plate) {
-  const response = await fetch(`${URL}/parking/${plate}`);
-  const data     = await response.json();
-
-  reservationsList.value = data.reservations;
-}
-
-async function checkin() {
-  if (plate.value === "" || plate.value.length !== 8) {
-    toast({
-      message: "Por favor, informe a placa do veículo",
-      type: "is-danger",
-      dismissible: true,
-      duration: 5000
-    });
-
-    return;
+    reservationsList.value = [] // Limpa lista de reservas.
+    carList.value = data;
   }
 
-  try {
-    const response = await fetch(`${URL}/parking/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ vehicle: { plate: plate.value } }),
-    });
+  async function getCar(plate) {
+    const response = await fetch(`${URL}/parking/${plate}`);
+    const data     = await response.json();
 
-    if (!response.ok) {
+    reservationsList.value = data.reservations;
+  }
+
+  async function checkin() {
+    if (plate.value === "" || plate.value.length !== 8) {
       toast({
-        message: `HTTP error ${response.status}`,
+        message: "Por favor, informe a placa do veículo",
         type: "is-danger",
         dismissible: true,
         duration: 5000
       });
-    } else {
+
+      return;
+    }
+
+    try {
+      const response = await fetch(`${URL}/parking/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ vehicle: { plate: plate.value } }),
+      });
+
+      if (!response.ok) {
+        toast({
+          message: `HTTP error ${response.status}`,
+          type: "is-danger",
+          dismissible: true,
+          duration: 5000
+        });
+      } else {
+        toast({
+          message: "Veículo registrado com sucesso",
+          type: "is-success",
+          dismissible: true,
+          duration: 5000
+        });
+
+        // const json = await response.json();
+        plate_input.value.value = "";
+        plate.value = "";
+        getCarList();
+      }
+    }
+    catch(error) {
       toast({
-        message: "Veículo registrado com sucesso",
-        type: "is-success",
+        message: `Não foi possível registrar esta reserva: ${error}`,
+        type: "is-danger",
         dismissible: true,
         duration: 5000
       });
-
-      // const json = await response.json();
-      plate_input.value.value = "";
-      plate.value = "";
-      getCarList();
     }
   }
-  catch(error) {
-    toast({
-      message: `Não foi possível registrar esta reserva: ${error}`,
-      type: "is-danger",
-      dismissible: true,
-      duration: 5000
-    });
+
+  async function payment({ code, plate }) {
+    await fetch(`${URL}/parking/${code}/pay`, { method: "PUT" });
+
+    toast({ message: 'Pagamento efetuado' })
+    getCar(plate);
   }
-}
 
-async function payment({ code, plate }) {
-  await fetch(`${URL}/parking/${code}/pay`, { method: "PUT" });
+  async function checkout({ code, plate }) {
+    await fetch(`${URL}/parking/${code}/out`, { method: "PUT" });
 
-  toast({ message: 'Pagamento efetuado' })
-  getCar(plate);
-}
+    toast({ message: 'Saída concluída' })
+    getCar(plate);
+  }
 
-async function checkout({ code, plate }) {
-  await fetch(`${URL}/parking/${code}/out`, { method: "PUT" });
-
-  toast({ message: 'Saída concluída' })
-  getCar(plate);
-}
-
-getCarList();
+  getCarList();
 </script>
