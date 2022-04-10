65b0e98a67267be6d8bd80db2a68db62006d103b
Valter Andrei <valterandrey@gmail.com>
2022-04-10 11:49:41 -0300

    Refactor timer counter

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -17,7 +17,7 @@
         <td>{{ reservation.code }}</td>
         <td>{{ formatDate(reservation.created_at) }}</td>
         <td>{{ formatDate(reservation.checkout) }}</td>
-        <td>{{ relativeTime(reservation) }}</td>
+        <td>{{ reservation.time }}</td>
         <td>
           <button
             v-if="!reservation.paid"
@@ -58,24 +58,4 @@
       ? new Date(date).toLocaleString('pt-BR')
       : '-'
   }
-
-  function relativeTime({ created_at, checkout }) {
-    const now = checkout == null ? new Date() : new Date(checkout)
-    const diff = now - new Date(created_at)
-    const seconds = Math.floor(diff / 1000)
-    const minutes = Math.floor(seconds / 60)
-    const hours = Math.floor(minutes / 60)
-    const days = Math.floor(hours / 24)
-
-    if (days > 0) {
-      return `${days} dia(s)`
-    } else if (hours > 0) {
-      return `${hours} hora(s)`
-    } else if (minutes > 0) {
-      return `${minutes} minuto(s)`
-    } else {
-      return `${seconds} segundo(s)`
-    }
-  }
-
 </script>
